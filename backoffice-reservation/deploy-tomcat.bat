cfbbad3bf39e02074277b38c423bb9c67ca685b5
Tommy-kamel <167763345+Tommy-kamel@users.noreply.github.com>
2026-03-11 09:55:21 +0300

    changement sprint4

--- backoffice-reservation/deploy-tomcat.bat
+++ backoffice-reservation/deploy-tomcat.bat
@@ -4,7 +4,7 @@ echo Build et deploiement sur Tomcat
 echo ========================================
 
 set TOMCAT_WEBAPPS=C:\apache-tomcat-10.1.28\webapps
-set WAR_NAME=backoffice-reservation.war
+set WAR_NAME=backoffice-reservation-sprint4.war
 
 echo.
 echo [1/2] Compilation du projet...
